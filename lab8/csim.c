4aafbff017d4c37b81d8f9ccfe311eaf8ed09730
zhaoshenglong <G245078728@sjtu.edu.cn>
2019-03-20 00:43:07 -0400

    :construction: get input now

--- lab8/csim.c
+++ lab8/csim.c
@@ -1,7 +1,50 @@
-#include "cachelab.h"
+/**
+ * INFORMATION For Lab8:
+ * 	Student name: zhaoshenglong
+ * 	Student ID  : 515030910241
+ */
 
-int main()
+#include "cachelab.h"
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+/* Align the address of blocks to be loaded*/
+#define ALIGN(p) ((((p) + block_n - 1) / block_n) * block_n)
+
+extern char *optarg;
+int main(int argc, char *argv[])
 {
-    printSummary(0, 0, 0);
+   	/* Number of sets, lines, blocks, misses, hit, evictions*/
+	int set_n = 1, line_n = 1, block_n = 1;	
+	int miss_n = 0, hit_n = 0, eviction_n = 0;
+	
+	/* Sign for verbose or silent*/
+	char verbose = 0;
+	
+	int opt;
+	/* Get options from stdin*/
+	while((opt = getopt(argc, argv, "-vs:E:b:t:")) != -1){
+		switch(opt){
+		case 'v':
+			verbose = 1;
+			break;	
+		case 's':
+			set_n = atoi(optarg);
+			break;
+		case 'E':
+			line_n = atoi(optarg);
+			break;
+		case 'b':
+			block_n = atoi(optarg);
+			break;
+		case 't':
+			break;
+		default:
+			printf("unknown character: %c\n", opt);
+		}
+	}
+	
+	printSummary(hit_n, miss_n, eviction_n);
     return 0;
 }
